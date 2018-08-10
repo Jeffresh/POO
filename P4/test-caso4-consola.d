02e588a98c73599f3644ea099322ac99889363d0
Jeff <jeff.uca@gmail.com>
2018-08-10 19:24:42 +0200

    Delete test-caso4-consola.d

--- P4/test-caso4-consola.d
+++
@@ -1,3 +0,0 @@
-test-caso4-consola.o test-caso4-consola.d : test-caso4-consola.cpp usuario.hpp ../P1/cadena.hpp \
-  tarjeta.hpp ../P1/fecha.hpp articulo.hpp pedido.hpp usuario-pedido.hpp \
-  pedido-articulo.hpp
