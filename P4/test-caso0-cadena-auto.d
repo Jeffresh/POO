88e8e42aabf678ba027c768fb8d6a1de4e5d2aab
Jeff <jeff.uca@gmail.com>
2018-08-10 19:20:37 +0200

    Delete test-caso0-cadena-auto.d

--- P4/test-caso0-cadena-auto.d
+++
@@ -1,3 +0,0 @@
-test-caso0-cadena-auto.o test-caso0-cadena-auto.d : test-caso0-cadena-auto.cpp test-auto.hpp \
-  ../P1/fct.h ../P1/fecha.hpp ../P1/cadena.hpp articulo.hpp tarjeta.hpp \
-  usuario.hpp pedido.hpp pedido-articulo.hpp usuario-pedido.hpp
